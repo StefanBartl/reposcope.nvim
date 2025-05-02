7b8f1a2fcc32d43d68c5f690b45ecbb59a23ec42
lavalue-coddeberg <bartl.stefan@pm.me>
2025-05-02 22:24:37 +0200

    11 feat(ui): add custom prompt input with protected prefix and icon   10    9 - Implemented manual prompt buffer instead of buftype = "prompt"    8 - Uses Nerd Font magnifier icon ( = U+F002) as prefix    7 - Sets initial cursor position after prompt prefix    6 - Blocks unwanted cursor movement and editing into prompt zone:    5   - Insert mode: blocks <Left>, <BS>, <C-w>, <Home>    4   - Normal mode: blocks `0` from jumping to column 0    3 - Uses `vim.fn.strdisplaywidth` to calculate display-safe prompt width    2 - Replaces default `prompt_setprompt()` with buffer-inserted prompt    1 - Ensures cursor enters insert mode via `vim.schedule(startinsert)`   12 - Design changes implemented (bg-color, border,..)    1

--- lua/reposcope/ui/start.lua
+++ lua/reposcope/ui/start.lua
@@ -1,4 +1,6 @@
 local M = {}
+-- TODO: Funktionenen auslagern: FP-Idiom
+-- TODO: Fumktionalitäten kapseln, zb set_bg()
 
 local width = math.floor(vim.o.columns * 0.6) -- total
 local height = math.floor(vim.o.lines * 0.8) -- total
@@ -19,6 +21,9 @@ local prompt_height = 1
 local on_enter = function(input)
   vim.notify("[reposcope.nvim] You searched: " .. input)
 end
+local magnifier = "\u{f002}"
+local prompt_prefix = " " .. magnifier .. "   "
+local prompt_len = vim.fn.strdisplaywidth(prompt_prefix)
 
 local list_height = math.floor(height * 0.6)
 local list_row = row + preview_height + prompt_height + 2
@@ -28,7 +33,7 @@ local list_lines = {
     "some/repo_3: Hier steht die Kurzbeschreibung"
   }
 
-local legend = "<Esc>: Quit   <Enter>: Search  <C-r> get Readme  <C-xx> xx"
+local legend = "<Esc>: Quit   <Enter>: Search  <C-r>: Readme  <?>: Keybindings"
 
 M.state = {
   buffers = {
@@ -36,30 +41,27 @@ M.state = {
     preview_buf = nil,
     prompt_buf = nil,
     list_buf = nil,
-    legend_buf = nil
   },
   windows = {
-    back_win = nil,
+    backg_win = nil,
     preview_win  = nil,
     prompt_win = nil,
     list_win = nil,
-    legend_win = nil
   }
 }
 
 function M.open_ui()
-  M.open_back_window()
+  --M.open_backg_window()
   --M.open_preview()
-  --M.open_prompt()
+  M.open_prompt()
   --M.open_list()
-  M.open_legend()
 end
 
-function M.open_back_window()
+function M.open_backg_window()
   M.state.buffers.back_buf = vim.api.nvim_create_buf(false, true)
   vim.api.nvim_buf_set_lines(M.state.buffers.back_buf, 0, -1, false, {})
 
-  M.state.back_win = vim.api.nvim_open_win(M.state.buffers.back_buf, false, {
+  M.state.windows.backg_win = vim.api.nvim_open_win(M.state.buffers.back_buf, false, {
     relative = "editor",
     col = col,
     row = row,
@@ -71,7 +73,9 @@ function M.open_back_window()
     style = "minimal",
     --focusable = false,
     noautocmd = true,
-    zindex = 10
+    zindex = 10,
+    footer = legend,
+    footer_pos = 'center',
   })
 
 end
@@ -80,7 +84,7 @@ function M.open_preview()
   M.state.buffers.pre_buf = vim.api.nvim_create_buf(false, true)
   vim.api.nvim_buf_set_lines(M.state.buffers.pre_buf, 0, -1, false, preview_lines)
 
-  M.state.preview_win = vim.api.nvim_open_win(M.state.buffers.pre_buf, false, {
+  M.state.windows.preview_win = vim.api.nvim_open_win(M.state.buffers.pre_buf, false, {
     relative = 'editor',
     col = col + padding,
     row = row + padding,
@@ -89,42 +93,91 @@ function M.open_preview()
     border = "none",
     title = "Preview",
     title_pos = 'center',
-    style = "minimal"
+    style = "minimal",
   })
 
 end
 
 function M.open_prompt()
   M.state.buffers.prompt_buf = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_buf_set_lines(M.state.buffers.prompt_buf, 0, -1, false, { "" })
 
-  M.state.prompt_win = vim.api.nvim_open_win(M.state.buffers.prompt_buf, true, {
-    relative = "editor",
-    row = row + preview_height,
-    col = col + 1,
-    width = width - 2,
-    height = prompt_height,
-    border = "single",
-    title = "Search Repositories",
-    title_pos = "center",
-    style = 'minimal'
+  local ns = vim.api.nvim_create_namespace("reposcope_prompt")
+  vim.api.nvim_set_hl(ns, "Normal", { background = '#8092b5', bg = '#252931' })
+
+  M.state.windows.prompt_win = vim.api.nvim_open_win(M.state.buffers.prompt_buf, true, {
+   relative = "editor",
+   row = row + preview_height,
+   col = col + 1,
+   width = width - 2,
+   height = prompt_height,
+   border = "single",
+   title = "Search Repositories",
+   title_pos = "center",
+   style = 'minimal',
   })
 
-  vim.bo[M.state.buffers.prompt_buf].buftype = "prompt"
-  vim.fn.prompt_setprompt(M.state.buffers.prompt_buf, "> ")
+  vim.api.nvim_win_set_hl_ns(M.state.windows.prompt_win, ns)
+  vim.api.nvim_buf_set_lines(M.state.buffers.prompt_buf, 0, -1, false, { prompt_prefix })
+  vim.api.nvim_win_set_cursor(M.state.windows.prompt_win, { 1, prompt_len })
+
+
+  -- === Block Left before prompt begin ====
+  vim.keymap.set("i", "<Left>", function()
+    local _, prompt_col = unpack(vim.api.nvim_win_get_cursor(0))
+    if prompt_col <= prompt_len then
+      return ""  -- blockiere Bewegung in den statischen Bereich
+    else
+      return "<Left>"
+    end
+  end, { buffer = M.state.buffers.prompt_buf, expr = true, silent = true })
+
+  -- === Block Backspace before prompt begin ===
+  vim.keymap.set("i", "<BS>", function()
+    local _, prompt_col = unpack(vim.api.nvim_win_get_cursor(0))
+    if prompt_col <= prompt_len then
+      return ""  -- nichts löschen
+    else
+      return "<BS>"
+    end
+  end, { buffer = M.state.buffers.prompt_buf, expr = true, silent = true })
+
+  -- === Block Word back before prompt begin ===
+  vim.keymap.set("i", "<C-w>", function()
+    local _, prompt_col = unpack(vim.api.nvim_win_get_cursor(0))
+    if prompt_col <= prompt_len then
+      return ""  -- nichts löschen
+    else
+      return "<C-w>"
+    end
+  end, { buffer = M.state.buffers.prompt_buf, expr = true, silent = true })
+
+  -- === Overwrite Home to prompt begin ===
+  vim.keymap.set("i", "<Home>", function()
+    return string.format("<Cmd>call cursor(1, %d)<CR>", prompt_len + 1)
+  end, { buffer = M.state.buffers.prompt_buf, expr = true, silent = true })
+
+  -- === Overwrite '0' ===
+  vim.keymap.set("n", "0", function()
+    vim.api.nvim_win_set_cursor(0, { 1, prompt_len })
+  end, { buffer = M.state.buffers.prompt_buf, silent = true })
+
 
   vim.keymap.set("i", "<CR>", function()
     local input = vim.api.nvim_get_current_line()
-    vim.api.nvim_buf_delete(M.state.buffers.prompt_buf, { force = true }) -- Eingabepuffer schließen
-    on_enter(input)  -- Callback ausführen
+    vim.api.nvim_buf_delete(M.state.buffers.prompt_buf, { force = true })
+    on_enter(input)
   end, { buffer = M.state.buffers.prompt_buf, silent = true })
+
+  vim.schedule(function()
+    vim.cmd("startinsert")
+  end)
 end
 
 function M.open_list()
   M.state.buffers.repo_buf = vim.api.nvim_create_buf(false, true)
   vim.api.nvim_buf_set_lines(M.state.buffers.repo_buf, 0, -1, false, list_lines)
 
-  M.state.list_win = vim.api.nvim_open_win(M.state.buffers.repo_buf, false, {
+  M.state.windows.list_win = vim.api.nvim_open_win(M.state.buffers.repo_buf, false, {
     relative = "editor",
     row = list_row,
     col = col,
@@ -133,27 +186,8 @@ function M.open_list()
     title = "Repositories",
     title_pos = "left",
     border = "none",
-    style = "minimal"
+    style = "minimal",
   })
- end
-
-function M.open_legend()
-  M.state.buffers.legend_buf = vim.api.nvim_create_buf(false, true)
-  local legend = require("reposcope.utils.text").center_text(legend, width)
-  vim.api.nvim_buf_set_lines(M.state.buffers.legend_buf, 0, -1, false, { legend })
-
-  M.state.windows.legend_win = vim.api.nvim_open_win(M.state.buffers.legend_buf, false, {
-    relative = 'editor',
-    col = col,
-    row = row + height - 1,
-    height = 1,
-    width = width,
-    border = "single",
-    title = "Command Legend",
-    title_pos = 'center',
-    style = 'minimal'
-  })
-
 end
 
 function M.close_ui()
