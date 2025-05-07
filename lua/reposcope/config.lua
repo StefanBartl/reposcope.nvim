48b6e6f110ab52da1accd4d5163638c65f8af920
lavalue-coddeberg <bartl.stefan@pm.me>
2025-05-07 20:17:27 +0200

    feat(config): add OS detection and dynamic state/cache path handling
    - Implemented OS detection in utils/utils.lua to distinguish between
    Windows and Unix-like systems.
    - Added dynamic state path (`g_state_path`) and cache path
    (`g_cache_path`) based on the detected OS.
      - Windows: `~/AppData/Local/nvim-data/reposcope`
      - Unix-like: `~/.local/state/nvim/reposcope`
    - Refactored configuration setup in config.lua to ensure paths are
    properly set and escaped.
    - Removed unnecessary state/system.lua file, integrating OS detection
    directly in utils/utils.lua.
    - Enhanced setup() function to automatically configure paths based on
    user options and OS type.
    - Added temprint debug function for fast deletion

--- lua/reposcope/config.lua
+++ lua/reposcope/config.lua
@@ -5,22 +5,44 @@
 ---@field is_debug_mode fun(): boolean Checks if debug mode is enabled
 local M = {}
 
+---Detects and sets the correct state path based on the OS
+---Uses the detected OS in the system state to determine the default path
+---@return string The state path for Reposcope
+local function set_state_path()
+  if require("reposcope.state.system").os == "unix" then
+    return vim.fn.expand("~/.local/state/nvim/reposcope")
+  else
+    return vim.fn.expand("~/AppData/Local/nvim-data/reposcope")
+  end
+end
+
 ---Default configuration options
 M.options = {
-  provider = "github",
-  preferred_requesters = { "gh", "curl", "wget" },
-  request_tool = "gh",
-  results_limit = 25, -- Github search api can return max 100 results without pagination
-  preview_limit = 200,
-  layout = "default",
-  dev_mode = false,
-  debug_mode = false,
+  provider = "github", -- Default provider for Reposcope (GitHub)
+  preferred_requesters = { "gh", "curl", "wget" }, -- Preferred tools for API requests
+  request_tool = "gh", -- Default request tool (GitHub CLI)
+  results_limit = 25, -- Default result limit for search queries
+  preview_limit = 200, -- Default preview limit for displayed results
+  layout = "default", -- Default UI layout
+  dev_mode = false, -- Developer mode flag
+  debug_mode = false, -- Debug mode flag
+  g_state_path = nil, -- Path for Reposcope state data
+  g_cache_path = nil, -- Cache path, determined in setup
 }
 
 ---Setup function for configuration
 ---@param opts table User configuration options
 function M.setup(opts)
+  -- Merge user-provided options with default options
   M.options = vim.tbl_deep_extend("force", M.options, opts or {})
+
+  -- Set and ensure the state path is properly set
+  M.options.g_state_path = vim.fn.fnameescape(
+    M.options.g_state_path or set_state_path()
+  )
+  M.options.g_cache_path = vim.fn.fnameescape(
+    M.options.g_cache_path or (M.options.g_state_path .. "/cache")
+  )
 end
 
 ---Checks if dev mode is enabled
