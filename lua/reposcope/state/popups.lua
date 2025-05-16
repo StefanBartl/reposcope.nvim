7c7820c3a94f327ceafef3eb418661a72eb7c230
lavalue-coddeberg <bartl.stefan@pm.me>
2025-05-16 21:01:07 +0200

    popups(state): Improved state functionality of the popups state

--- lua/reposcope/state/popups.lua
+++ lua/reposcope/state/popups.lua
@@ -1,5 +1,6 @@
 ---@class PopupsStateManager
 ---@field CloneModule ClonePopupState State for the providers/clone.lua module --NOTE: This is how annotation could be
+---@field ensure_clean_state fun(tbl: table): nil Ensures the state of a given table is clean
 local M = {}
 
 ---@class ClonePopupState
@@ -17,5 +18,26 @@ M.stats = {
   buf = nil,
   win = nil,
 }
+
+---Ensures the state of a given table is clean
+--- - Unmaps all keymaps for the buffer
+--- - Closes the window
+--- - Resets the buffer and window to nil
+---@param tbl table The table to ensure is clean
+---@return nil
+function M.ensure_clean_state(tbl)
+  if type(tbl) ~= "table" then
+    return
+  end
+  if tbl.buf and vim.api.nvim_buf_is_valid(tbl.buf) then
+    require("reposcope.keymaps").force_unmap_all(tbl.buf)
+    vim.api.nvim_buf_delete(tbl.buf, { force = true })
+  end
+
+  tbl = {
+    buf = nil,
+    win = nil,
+  }
+end
 
 return M
